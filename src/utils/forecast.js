3764a073d9ceeedbb3afe78c88b684ba0e6ef07e
Abdou <abderrahmanesiahmed@gmail.com>
2021-06-06 11:53:10 +0100

    Add new data in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error) {
             callback('Unable to find location', undefined)
         }else {
-            callback(undefined, body.current.weather_descriptions + '. its currently '+ body.current.temperature + ' degrees out. There is a '+ body.current.precip + '% chance of rain')
+            callback(undefined, body.current.weather_descriptions + '. its currently '+ body.current.temperature + ' degrees out and the Humidity is '+ body.current.humidity + '%. There is a '+ body.current.precip + '% chance of rain')
         }
     })
 }
